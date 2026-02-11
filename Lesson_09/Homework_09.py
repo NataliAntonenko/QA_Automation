1f3503ae3e6d30c93e69c85a6d10dd7197e9c10a
NataliaAntonenko <nata@night.kharkov.ua>
2026-02-11 16:44:50 +0200

    Homework_09

---
+++ Lesson_09/Homework_09.py
@@ -0,0 +1,40 @@
+"""
+Створіть клас геометричної фігури "Ромб". Клас повинен мати наступні атрибути:
+
+сторона_а (довжина сторони a).
+кут_а (кут між сторонами a і b).
+кут_б (суміжний з кутом кут_а).
+Необхідно реалізувати наступні вимоги:
+
+Значення сторони сторона_а повинно бути більше 0.
+Кути кут_а та кут_б повинні задовольняти умову: кут_а + кут_б = 180
+Протилежні кути ромба завжди рівні, тому при заданому значенні кут_а, значення кут_б обчислюється автоматично.
+Для встановлення значень атрибутів використовуйте метод __setattr__.
+"""
+
+
+class Rhombus:
+    def __init__(self, side_a, angle_a):
+        self.side_a = side_a
+        self.angle_a = angle_a
+
+    def __setattr__(self, name, value):
+        if name == "side_a":
+            if value <= 0:
+                raise ValueError("Сторона ромба повинна бути більше 0.")
+
+        if name == "angle_a":
+            if value <= 0 or value >= 180:
+                raise ValueError("Кут angle_a повинен бути між 0 і 180 градусами.")
+
+            super().__setattr__(name, value)
+            super().__setattr__("angle_b", 180 - value)
+            return
+
+        if name == "angle_b":
+            raise AttributeError("Кут angle_b обчислюється автоматично.")
+
+        super().__setattr__(name, value)
+
+    def __str__(self):
+        return f"Ромб: сторона = {self.side_a}, кут_a = {self.angle_a}, кут_b = {self.angle_b}"
